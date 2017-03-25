23c7dd0d2ac6fd217a83556a6b25a707bf8becc7
niki <24849691+niki@users.noreply.github.com>
2017-03-25 19:18:44 +0900

    sakura-r4175	Imp: キーワードヘルプメニューのオプション [patchunicode:#1066] Contributed by Moca

--- sakura_core/sakura_rc.h
+++ sakura_core/sakura_rc.h
@@ -788,6 +788,7 @@
 #define IDC_CHECK_INDENTCPPSTR          1719
 #define IDC_CHECK_INDENTCPPCMT          1720
 #define IDC_CHECK_INDENTCPPUNDO         1721
+#define IDC_STATIC_MENU                 1723
 #define IDS_AUTHOR_PAGE                 4054
 #define IDS_ABOUT_DESCRIPTION           4056
 #define IDD_TYPELIST                    5000
@@ -843,7 +844,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        230
 #define _APS_NEXT_COMMAND_VALUE         4057
-#define _APS_NEXT_CONTROL_VALUE         1722
+#define _APS_NEXT_CONTROL_VALUE         1724
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
