73d697b682209a9a33e90acd79b7e7853df2b368
yuu_zuki <24849691+niki@users.noreply.github.com>
2026-08-10 11:42:51 +0900

    update

--- sakura_core/my_config.h
+++ sakura_core/my_config.h
@@ -57,7 +57,7 @@
 // lf.lfHeight = DpiPointsToPixels(-10); // 高DPI対応（ポイント数から算出）
 
 // 修正者
-#define NKMM_AUTHOR       "CAT-ech Co."
+#define NKMM_AUTHOR       "Yu-zuki."
 #define NKMM_AUTHOR_PAGE  "https://github.com/niki/sakura"
 
 // 拡張用レジストリキー
